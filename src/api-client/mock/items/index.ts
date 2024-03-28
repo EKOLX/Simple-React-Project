fbbec5496bdafbe9956fbc46a61dcaccc3b1b0c0
EL Khan <elxanmursal@gmail.com>
2024-03-28 10:34:00 +0400

    Added and updated env configs.

--- src/api-client/mock/items/index.ts
+++ src/api-client/mock/items/index.ts
@@ -7,6 +7,7 @@ import {
 const options: ItemsApiClientOptions = {
   endpoints: {
     fetchItems: "/static/mock-data/items/items.json",
+    // fetchItems: "/jsonserver/items",
   },
   mockDelay: 1000,
 };
--- vite.config.prod.ts
+++ vite.config.prod.ts
@@ -3,11 +3,18 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   envDir: "./src/",
+  resolve: {
+    alias: {
+      // @ts-ignore
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
